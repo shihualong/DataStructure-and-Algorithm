941ba3233692a5aa40e661f3b5c0e505a39b708e
Carol <475880692@qq.com>
2018-05-13 23:54:02 +0800

    循环链表  -- 约瑟夫问题

---
+++ DataStructure-and-Algorithm/Link/CirculationLinkList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Link
+{
+    class CirculationLinkList
+    { 
+        class Node
+        {
+            public object item;
+            public CirculationLinkList.Node next;
+            public override string ToString()
+            {
+                return item.ToString();
+            }
+            public Node(object v)
+            {
+                item = v;
+                next = null;
+            }
+        }
+        private int count;
+        private Node tail;
+        private Node curPreValue;
+
+        public int Count { get { return count; } }
+
+        public void Add(object value)
+        {
+            Node newNode = new Link.CirculationLinkList.Node(value);
+            if (tail == null)
+            {
+                tail = newNode;
+                tail.next = newNode;
+                curPreValue = newNode;
+            }
+            else
+            {
+                newNode.next = tail.next;
+                tail.next = newNode;
+                if (curPreValue == tail)
+                {
+                    curPreValue = newNode;
+                }
+                tail = newNode;
+                
+            }
+            count++;
+        }
+
+        public void RemoveCurrNode()
+        {
+            if (tail == null) return;
+            else if ( count == 1)
+            {
+                tail = null;
+                curPreValue = null;
+            }
+            else
+            {
+             
+                curPreValue.next = curPreValue.next.next;
+            }
+            count--;
+        }
+        public void Move(int s)
+        {
+            for (int i = 0; i < s; i++)
+            {
+                curPreValue = curPreValue.next;
+            }
+        }
+        public object Current
+        {
+            get
+            {
+                return curPreValue.next.item;
+            }
+        }
+        public override string ToString()
+        {
+            if (tail == null) return string.Empty;
+            string s = "";
+            Node temp = tail.next;
+            for (int i = 0; i < count; i++)
+            {
+                s += temp.ToString() + "   ";
+                temp = temp.next;
+            }
+            return s;
+
+        }
+    }
+}
---
+++ DataStructure-and-Algorithm/Link/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Link
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CirculationLinkList lst = new CirculationLinkList();
+            #region Test
+            //lst.Add(1);
+            //lst.Add(2);
+            //lst.Add(3);
+            //lst.Add(4);
+            //lst.Add(5);
+            //lst.Add(6);
+
+            //Console.WriteLine(lst.ToString());
+
+            //Console.WriteLine(lst.Current.ToString());
+
+
+            //lst.RemoveCurrNode();
+            //Console.WriteLine(lst.ToString());
+
+            //lst.Move(2);
+            //Console.WriteLine(lst.Current.ToString());
+            //Console.WriteLine(lst.ToString());
+            #endregion
+            #region 约瑟夫
+            string s = string.Empty;
+            Console.WriteLine("请输出总数：");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("每次报到第几");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("开始报数");
+            for (int i = 1; i <= count; i++)
+            {
+                lst.Add(i);
+            }
+            Console.WriteLine(lst.ToString());
+
+            while (lst.Count > 15)
+            {
+                lst.Move(m - 1);
+                s += lst.Current.ToString() + "   ";
+                lst.RemoveCurrNode();
+                Console.WriteLine(lst.Current + "开始报数");
+            }
+            Console.WriteLine("被扔入大海的人为：" + s );
+
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
