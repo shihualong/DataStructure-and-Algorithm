3db87ce35ad0d19ae497922ba9ffa23cb5f1f2e9
Carol <475880692@qq.com>
2018-05-13 21:47:25 +0800

    ArrayList

---
+++ DataStructure-and-Algorithm/DataStructure-and-Algorithm/MyArrayList.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure_and_Algorithm
+{
+    class MyArrayList
+    {
+        private const int defaultCapacity = 4;
+        private int count;
+        private static readonly object[] emptyCapacity = new object[0];
+        private object[] items;
+
+        public MyArrayList()
+        {
+            items = emptyCapacity;
+        }
+        public MyArrayList(int c)
+        {
+            items = new object[c];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+        public object this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.count)
+                    throw new ArgumentOutOfRangeException("index", "OutOfRange");
+                return items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= this.count)
+                    throw new ArgumentOutOfRangeException("index", "OutOfRange");
+                items[index] = value;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return items.Length;
+            }
+            set
+            {
+                if (value < this.count)
+                {
+                    Console.WriteLine("Capacity:{0} -- Count :{1}", value, count);
+                    throw new ArgumentOutOfRangeException("Capacity", "OutOfRange");
+                }
+                if (value != this.items.Length)
+                {
+                    if (value > 0)
+                    {
+                        object[] destinationArray = new object[value];
+                        if (this.count > 0)
+                        {
+                            Array.Copy(items, 0, destinationArray, 0, count);
+                        }
+                        items = destinationArray;
+                    }
+                    else
+                    {
+                        items = new object[defaultCapacity];
+                    }
+                }
+
+            }
+        }
+
+        public int Add(object v)
+        {
+            if (this.count == this.items.Length)
+            {
+                EnsureCapacity(count + 1);
+            }
+            this.items[count] = v;
+            int tempCount = count;
+            count = tempCount + 1;
+            return tempCount;
+
+
+        }
+        private void EnsureCapacity(int v)
+        {
+            if (this.items.Length < v)
+            {
+                int num = (this.items.Length == 0) ? defaultCapacity : (this.items.Length * 2);
+
+                this.Capacity = num;
+            }
+        }
+        public void Remove(int index)
+        {
+            if (index < 0 || index >= this.count)
+                throw new ArgumentOutOfRangeException("index", "OutOfRange");
+
+            count--;
+            if (index < count)
+            {
+                Array.Copy(items, index + 1, items, index, this.count - index);
+            }
+            this.items[index] = null;
+        }
+        public void Insert(int index,object v)
+        {
+            if (index < 0 || index > this.count)
+                throw new ArgumentOutOfRangeException("index", "OutOfRange");
+            if (this.count == items.Length)
+                EnsureCapacity(count + 1);
+            if (index < count)
+            {
+                Array.Copy(items, index, items, index + 1, count - index);
+            }
+            this.items[index] = v;
+        }
+
+        public void TrimToCount()
+        {
+            this.Capacity = count;
+        }
+        public object Local(int index)
+        {
+            try
+            {
+                if (index < 0 || index >= this.count)
+                    throw new ArgumentOutOfRangeException("index", "outofRange");
+                return items[index];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Count:{0},Capacity:{1},LastIndex:{2}",count,Capacity,count - 1);
+                return null;
+            }
+          
+        }
+    }
+}
--- DataStructure-and-Algorithm/DataStructure-and-Algorithm/Program.cs
+++ DataStructure-and-Algorithm/DataStructure-and-Algorithm/Program.cs
@@ -10,6 +10,19 @@ namespace DataStructure_and_Algorithm
     {
         static void Main(string[] args)
         {
+
+            #region MyRegion
+            MyArrayList arr = new MyArrayList();
+            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+            arr.Add(1);
+            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+            for (int i = 0; i < 6; i++)
+            {
+                arr.Add(i);
+            }
+            Console.WriteLine("arr当前的容量为：" + arr.Capacity + "  长度为：" + arr.Count);
+            Console.WriteLine(arr.Local(6));
+            #endregion
         }
     }
 }
